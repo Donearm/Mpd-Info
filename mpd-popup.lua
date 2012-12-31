9e4834c1a1eca75b3dc43dbf37c31eaf2400e034
Gianluca Fiore <forod.g@gmail.com>
2012-12-31 13:41:13 +0100

    update copyright year

--- mpd-popup.lua
+++ mpd-popup.lua
@@ -3,7 +3,7 @@
 --- Popup with info about current playing song in mpd.
 -- To be loaded from awesomeWM's rc.lua
 -- @author Gianluca Fiore
--- @copyright 2011-2012, Gianluca Fiore <forod.g@gmail.com>
+-- @copyright 2011-2013, Gianluca Fiore <forod.g@gmail.com>
 --
 -- Requires lua-mpd (https://github.com/silentbicycle/lua-mpd)
 -- and Lua FileSystem 
