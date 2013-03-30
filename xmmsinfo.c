9693d0af042053f4b9f160089625220d12ea3fe0
Gianluca Fiore <forod.g@gmail.com>
2013-03-30 16:22:54 +0100

    first integration of xmmsinfo into lua

--- xmmsinfo.c
+++ xmmsinfo.c
@@ -4,71 +4,8 @@
 
 lua_State* L;
 
-void get_mediainfo(xmmsc_connection_t *connection, int id, lua_State *L)
+int show_xmmsinfo_c(lua_State *L)
 {
-	xmmsc_result_t *result;
-	xmmsv_t *return_value;
-	const char *err_buf;
-
-	xmmsv_t *dict_entry;
-	xmmsv_t *infos;
-	const char *val;
-
-	result = xmmsc_medialib_get_info(connection, id);
-	xmmsc_result_wait(result);
-	return_value = xmmsc_result_get_value(result);
-
-	if(xmmsv_get_error(return_value, &err_buf)) {
-		fprintf(stderr, "Medialib returns error, %s\n",
-				err_buf);
-		exit(EXIT_FAILURE);
-	}
-
-	infos = xmmsv_propdict_to_dict(return_value, NULL);
-	if(!xmmsv_dict_get(infos, "artist", &dict_entry) ||
-			!xmmsv_get_string(dict_entry, &val)) {
-		val = "No Artist";
-	}
-
-	lua_pushstring(L, val);
-	printf("Artist = %s\n", val);
-
-	if(!xmmsv_dict_get(infos, "album", &dict_entry) ||
-			!xmmsv_get_string(dict_entry, &val)) {
-		val = "No Album";
-	}
-
-	lua_pushstring(L, val);
-	printf("Album = %s\n", val);
-
-	if(!xmmsv_dict_get(infos, "title", &dict_entry) ||
-			!xmmsv_get_string(dict_entry, &val)) {
-		val = "No Title";
-	}
-
-	lua_pushstring(L, val);
-	printf("Title = %s\n", val);
-
-	lua_pushinteger(L, id);
-	printf("Id = %d\n", id);
-
-	if(!xmmsv_dict_get(infos, "url", &dict_entry) ||
-			!xmmsv_get_string(dict_entry, &val)) {
-		val = NULL;
-	}
-
-	lua_pushstring(L, val);
-	printf("Uri = %s\n", val);
-
-	xmmsv_unref(infos);
-	xmmsc_result_unref(result);
-}
-
-
-
-int main (int argc, char **argv, lua_State *L)
-{
-
 	xmmsc_connection_t *connection;
 	xmmsc_result_t *state;
 	xmmsv_t *state_value;
@@ -82,14 +19,14 @@ int main (int argc, char **argv, lua_State *L)
 	connection = xmmsc_init("xmmsinfo");
 	if (!connection) {
 		fprintf(stderr, "No connection!\n");
-		exit(EXIT_FAILURE);
+		return(EXIT_FAILURE);
 	}
 
 	// try to connect
 	if (!xmmsc_connect(connection, getenv("XMMS_PATH"))) {
 		fprintf(stderr, "Connection failed, %s\n",
 				xmmsc_get_last_error(connection));
-		exit(EXIT_FAILURE);
+		return(EXIT_FAILURE);
 	}
 
 	// get current xmms2 status
@@ -118,8 +55,6 @@ int main (int argc, char **argv, lua_State *L)
 		state_str = "stopped";
 	}
 
-	// register mediainfo function to lua
-	lua_register(L, "getmediainfo", get_mediainfo);
 	// push state_str to lua
 	lua_pushstring(L, state_str);
 
@@ -135,8 +70,64 @@ int main (int argc, char **argv, lua_State *L)
 
 	printf("Current song id is, %d\n", cur_id);
 
-	get_mediainfo(connection, cur_id, L);
 
+	// initialize variables for the song info
+	xmmsc_result_t *result;
+	xmmsv_t *return_value;
+	//const char *err_buf;
+	xmmsv_t *dict_entry;
+	xmmsv_t *infos;
+	const char *val;
+
+	result = xmmsc_medialib_get_info(connection, cur_id);
+	xmmsc_result_wait(result);
+	return_value = xmmsc_result_get_value(result);
+
+	if(xmmsv_get_error(return_value, &err_buf)) {
+		fprintf(stderr, "Medialib returns error, %s\n",
+				err_buf);
+		return(EXIT_FAILURE);
+	}
+
+	infos = xmmsv_propdict_to_dict(return_value, NULL);
+	if(!xmmsv_dict_get(infos, "artist", &dict_entry) ||
+			!xmmsv_get_string(dict_entry, &val)) {
+		val = "No Artist";
+	}
+
+	lua_pushstring(L, val);
+	printf("Artist = %s\n", val);
+
+	if(!xmmsv_dict_get(infos, "album", &dict_entry) ||
+			!xmmsv_get_string(dict_entry, &val)) {
+		val = "No Album";
+	}
+
+	lua_pushstring(L, val);
+	printf("Album = %s\n", val);
+
+	if(!xmmsv_dict_get(infos, "title", &dict_entry) ||
+			!xmmsv_get_string(dict_entry, &val)) {
+		val = "No Title";
+	}
+
+	lua_pushstring(L, val);
+	printf("Title = %s\n", val);
+
+	lua_pushinteger(L, cur_id);
+	printf("Id = %d\n", cur_id);
+
+	if(!xmmsv_dict_get(infos, "url", &dict_entry) ||
+			!xmmsv_get_string(dict_entry, &val)) {
+		val = NULL;
+	}
+
+	lua_pushstring(L, val);
+	printf("Uri = %s\n", val);
+
+	// clean up
+	xmmsv_unref(infos);
+	xmmsc_result_unref(result);
 	xmmsc_result_unref(state);
 	xmmsc_result_unref(current_id);
 	xmmsc_unref(connection);
@@ -144,3 +135,8 @@ int main (int argc, char **argv, lua_State *L)
 }
 
 
+int luaopen_xmmsinfo(lua_State *L)
+{
+	lua_register(L, "showxmmsinfo", show_xmmsinfo_c);
+	return 1;
+}
