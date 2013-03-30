41955b0a554f33bbc0e3bad7748bb3c050686e42
Gianluca Fiore <forod.g@gmail.com>
2013-03-30 12:04:52 +0100

    initial support for xmms2

---
+++ xmmsinfo.c
@@ -0,0 +1,169 @@
+#include <stdlib.h>
+#include <xmmsclient/xmmsclient.h>
+#include <lua.h>
+
+lua_State* L;
+
+void get_mediainfo(xmmsc_connection_t *connection, int id, lua_State *L)
+{
+	xmmsc_result_t *result;
+	xmmsv_t *return_value;
+	const char *err_buf;
+
+	xmmsv_t *dict_entry;
+	xmmsv_t *infos;
+	const char *val;
+
+	result = xmmsc_medialib_get_info(connection, id);
+	xmmsc_result_wait(result);
+	return_value = xmmsc_result_get_value(result);
+
+	if(xmmsv_get_error(return_value, &err_buf)) {
+		fprintf(stderr, "Medialib returns error, %s\n",
+				err_buf);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("Id = %d\n", id);
+
+	infos = xmmsv_propdict_to_dict(return_value, NULL);
+	if(!xmmsv_dict_get(infos, "artist", &dict_entry) ||
+			!xmmsv_get_string(dict_entry, &val)) {
+		val = "No Artist";
+	}
+
+	printf("Artist = %s\n", val);
+
+	if(!xmmsv_dict_get(infos, "title", &dict_entry) ||
+			!xmmsv_get_string(dict_entry, &val)) {
+		val = "No Title";
+	}
+
+	printf("Title = %s\n", val);
+
+	if(!xmmsv_dict_get(infos, "album", &dict_entry) ||
+			!xmmsv_get_string(dict_entry, &val)) {
+		val = "No Album";
+	}
+
+	printf("Album = %s\n", val);
+
+	if(!xmmsv_dict_get(infos, "url", &dict_entry) ||
+			!xmmsv_get_string(dict_entry, &val)) {
+		val = NULL;
+	}
+
+	printf("Uri = %s\n", val);
+
+	xmmsv_unref(infos);
+	xmmsc_result_unref(result);
+}
+
+
+
+int main (int argc, char **argv)
+{
+
+	xmmsc_connection_t *connection;
+	xmmsc_result_t *result;
+	xmmsc_result_t *state;
+	xmmsv_t *return_value;
+	xmmsv_t *state_value;
+	const char *err_buf;
+	int32_t status;
+
+	/* variables that we'll need later */
+	xmmsv_list_iter_t *it;
+
+	// initialize the connection
+	connection = xmmsc_init("xmmsinfo");
+	if (!connection) {
+		fprintf(stderr, "No connection!\n");
+		exit(EXIT_FAILURE);
+	}
+
+	// try to connect
+	if (!xmmsc_connect(connection, getenv("XMMS_PATH"))) {
+		fprintf(stderr, "Connection failed, %s\n",
+				xmmsc_get_last_error(connection));
+		exit(EXIT_FAILURE);
+	}
+
+	// get current xmms2 status
+	state = xmmsc_playback_status(connection);
+	xmmsc_result_wait(state);
+	state_value = xmmsc_result_get_value(state);
+
+	if(xmmsv_get_error(state_value, &err_buf)) {
+		fprintf(stderr, "Error while asking for the connection status, %s\n",
+				err_buf);
+	}
+
+	if(!xmmsv_get_int(state_value, &status)) {
+		fprintf(stderr, "Couldn't get connection status, %d\n", status);
+	}
+
+	// 0 == stopped; 1 == playing; 2 == paused
+	if(status == XMMS_PLAYBACK_STATUS_PLAY) {
+		printf("Status is playing, %d\n", status);
+	} else if (status == XMMS_PLAYBACK_STATUS_PAUSE) {
+		printf("Status is paused, %d\n", status);
+	} else {
+		printf("Status is stopped, %d\n", status);
+	}
+
+	// get current position in the playlist
+	xmmsc_result_t *current_pos;
+	xmmsv_t *current_pos_value;
+	int32_t cur_pos;
+
+	current_pos = xmmsc_playlist_current_pos(connection, XMMS_ACTIVE_PLAYLIST);
+	xmmsc_result_wait(current_pos);
+	current_pos_value = xmmsc_result_get_value(current_pos);
+	xmmsv_get_int(current_pos_value, &cur_pos);
+
+	printf("Current position is, %d\n", cur_pos);
+
+	// ask for the playlist
+	result = xmmsc_playlist_list_entries(connection, NULL);
+	xmmsc_result_wait(result);
+	return_value = xmmsc_result_get_value(result);
+
+	if(xmmsv_get_error(return_value, &err_buf)) {
+		fprintf(stderr, "Error while asking for the playlist, %s\n",
+				err_buf);
+		exit(EXIT_FAILURE);
+	}
+
+
+	// extract the iterator over the list
+	if(!xmmsv_get_list_iter(return_value, &it)) {
+		fprintf(stderr, "xmmsc_playlist_list_entries didn't \
+				return a list as expected\n");
+		exit(EXIT_FAILURE);
+	}
+
+	// loop over the list
+	for(; xmmsv_list_iter_valid(it); xmmsv_list_iter_next(it)) {
+		int id;
+		xmmsv_t *list_entry;
+
+		if(!xmmsv_list_iter_entry(it, &list_entry)) {
+			fprintf(stderr, "Couldn't get entry from list\n");
+			exit(EXIT_FAILURE);
+		}
+		// extract the int from the entry
+		if(!xmmsv_get_int(list_entry, &id)) {
+			fprintf(stderr, "Couldn't get int from list entry\n");
+			exit(EXIT_FAILURE);
+		}
+
+		get_mediainfo(connection, id, L);
+	}
+
+	xmmsc_result_unref(result);
+	xmmsc_unref(connection);
+	return(EXIT_SUCCESS);
+}
+
+
